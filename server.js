cd5d7071e1bb58d7c47becde46683133c1c1a056
lyes <abc@example.com>
2020-12-18 00:07:52 +0100

    committing API

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require("express")
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser:true})
+const db = mongoose.connection
+db.on('error', (error)=> console.error(error))
+db.on('open', ()=> console.log("Connected to database"))
+
+app.listen(8888, () => {
+    console.log("server running on port 8888")
+}) 
